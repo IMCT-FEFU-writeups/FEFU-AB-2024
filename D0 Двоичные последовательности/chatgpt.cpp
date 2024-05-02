74689db8b98ca9190a7539f61ddc188180e59c89
Beaver <beavernotacat@gmail.com>
2024-05-02 14:12:02 +1000

    Update chatgpt.cpp

--- D0 Двоичные последовательности/chatgpt.cpp
+++ D0 Двоичные последовательности/chatgpt.cpp
@@ -1,27 +1,21 @@
-/*
- * Двоичные последовательности прибавлением 1 к числу в двоичной системе счисления. Не уверен что примут, тк работаем не через бити, но я рот ебал
- */
-
-#include <iostream>
+#include <fstream>
 #include <string>
-#include <queue>
 
 using namespace std;
+ifstream cin("input.txt");
+ofstream cout("output.txt");
 
 void generateBinary(int N) {
-    queue<string> sequences;
-    sequences.push("0");
-    sequences.push("1");
-
-    while (!sequences.empty()) {
-        string current = sequences.front();
-        sequences.pop();
-        if (current.length() == N) {
-            cout << current << endl;
-        } else {
-            sequences.push(current + "0");
-            sequences.push(current + "1");
+    for (int i = 0; i < (1 << N); ++i) {
+        string binary = "";
+        for (int j = N - 1; j >= 0; --j) {
+            if (i & (1 << j)) {
+                binary += '1';
+            } else {
+                binary += '0';
+            }
         }
+        cout << binary << endl;
     }
 }
 
